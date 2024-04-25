9ec263720512ddbdb44f4e98631585e95a37592b
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-04-25 10:56:56 -0300

    :recycle: refactor: Ajustes na implementação.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,46 +2,86 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UserController extends Controller
 {
+    protected $users;
+
+    public function __construct(User $users)
+    {
+        $this->user = $users;
+    }
+
     public function index()
     {
-        $users = User::all();
+        $users = $this->user->all();
         return response()->json($users);
     }
 
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:6',
-        ]);
-
-        $user = User::create($request->all());
-        return response()->json($user, 201);
+    public function show($id){
+        try{
+            $users = $this->user->findOrFail($id);
+            return response()->json($users);
+        } catch (\Exception $e) {
+            return response()->json([
+                'info' => 'error',
+                'result' => 'Não foi possível encontrar o usuário informado!',
+                'error' => $e->getMessage(),
+            ], 400);
+        }
     }
 
-    public function update(Request $request, $id)
+    public function store(UserRequest $request)
     {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email,'.$id,
-            'password' => 'required|min:6',
-        ]);
+        try {
+            $user = $this->user->create($request->all());
+            return response()->json($user, 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'info' => 'error',
+                'result' => 'Não foi possível cadastrar um novo usuário!',
+                'error' => $e->getMessage(),
+            ], 400);
+        }
+    }
 
-        $user = User::findOrFail($id);
-        $user->update($request->all());
-        return response()->json($user, 200);
+    public function update(UserRequest $request, $id)
+    {
+        try {
+            $user = $this->user->findOrFail($id);
+
+            $validatedData = $request->validated();
+
+            $user->name = $validatedData['name'];
+            $user->email = $validatedData['email'];
+            $user->password = $validatedData['password'];
+            $user->update();
+
+            return response()->json($user, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'info' => 'error',
+                'result' => 'Não foi possível atualizar o usuário informado!',
+                'error' => $e->getMessage(),
+            ], 400);
+        }
     }
 
     public function destroy($id)
     {
-        $user = User::findOrFail($id);
-        $user->delete();
-        return response()->json(null, 204);
+        try {
+            $user = $this->user->findOrFail($id);
+            $user->delete();
+
+            return response()->json(null, 204);
+        } catch (\Exception $e) {
+            return response()->json([
+                'info' => 'error',
+                'result' => 'Não foi possível excluir o usuário informado!',
+                'error' => $e->getMessage(),
+            ], 400);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
 Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
