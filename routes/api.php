a2b7ab5859216196bece20f54b6530b92d17c8b1
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-04-25 23:34:42 -0300

    :recycle: refactor: Ajustadas rotas de usuário

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/register', [UserController::class, 'register']);
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users', [UserController::class, 'showAll']);
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::put('/users/{id}', [UserController::class, 'update']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
 });
-
-Route::post('/users', [UserController::class, 'store']);
-
-Route::post('/login', [AuthController::class, 'login']);
